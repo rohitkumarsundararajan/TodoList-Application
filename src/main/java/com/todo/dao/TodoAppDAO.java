f814f12e4075fd5cda4907d858dff6d933a95684
Dheenath Sundararajan <rohitkumarsundararajan@gmail.com>
2025-12-26 21:38:21 +0530

    fix addTodo to return generated id and load correctly

--- src/main/java/com/todo/dao/TodoAppDAO.java
+++ src/main/java/com/todo/dao/TodoAppDAO.java
@@ -12,6 +12,7 @@ import com.todo.util.DatabaseConnection;
 
 public class TodoAppDAO {
     private static final String GET_TODOS = "SELECT * FROM todos";
+    private static final String ADD_TODO = "INSERT INTO todos (title,description,completed,created_At,updated_At) VALUES (?,?,?,?,?)";
 
     public List<Todo> getAllTodos() throws SQLException{
         List<Todo> todos = new ArrayList<>();
@@ -36,6 +37,33 @@ public class TodoAppDAO {
             }
             return todos;
         }
+
+    public void addTodo(Todo todo) throws SQLException {
+    try (Connection conn = DatabaseConnection.getDBConnection();
+         PreparedStatement stmt =
+            conn.prepareStatement(ADD_TODO, PreparedStatement.RETURN_GENERATED_KEYS)) {
+
+        stmt.setString(1, todo.getTitle());
+        stmt.setString(2, todo.getDescription());
+        stmt.setBoolean(3, todo.isCompleted());
+        stmt.setObject(4, todo.getCreatedAt());
+        stmt.setObject(5, todo.getUpdatedAt());
+
+        int rowsAffected = stmt.executeUpdate();
+
+        if (rowsAffected == 0) {
+            throw new SQLException("Adding todo failed, no rows affected.");
+        }
+
+        try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+            if (generatedKeys.next()) {
+                todo.setId(generatedKeys.getInt(1));
+            } else {
+                throw new SQLException("Adding todo failed, no ID obtained.");
+            }
+        }
+        }
+    }
 
 
 }
